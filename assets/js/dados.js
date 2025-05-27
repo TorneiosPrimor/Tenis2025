a06a20d3568faca1b3fdd05c15d80dafbf92c563
TorneiosPrimor <g.araujoveroneze@gmail.com>
2025-05-27 08:30:18 -0400

    Atualizar o dados.js

--- assets/js/dados.js
+++ assets/js/dados.js
@@ -29,7 +29,7 @@ const dadosCampeonato = {
         { jogador1: 'Mayke', jogador2: 'Jefferson', sets: [] },
         { jogador1: 'Mayke', jogador2: 'Bittencourt', sets: [[0, 6], [1, 6]] },
         { jogador1: 'Adriano', jogador2: 'Ceará', sets: [] },
-        { jogador1: 'Adriano', jogador2: 'Nero', sets: [] },
+        { jogador1: 'Adriano', jogador2: 'Nero', sets: [[6, 4], [6, 4]] },
         { jogador1: 'Adriano', jogador2: 'Jefferson', sets: [[3, 6], [6, 1], [7, 3]] },
         { jogador1: 'Adriano', jogador2: 'Bittencourt', sets: [] },
         { jogador1: 'Ceará', jogador2: 'Nero', sets: [] },
